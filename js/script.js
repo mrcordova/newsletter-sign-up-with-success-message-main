773905e18ae936dcd42fabc482002b9fbddb5ae8
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-08 20:21:00 -0700

    add validation

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const submitBtn = document.querySelector('.sub-btn');
 const dismissBtn = document.querySelector('.dismiss-btn');
+const inputBtn = document.querySelector('.input-textbox'); 
+const emailError = document.getElementById('email-error');
+
 
 submitBtn.addEventListener('click', (e) => {
     // validate emial here
@@ -8,11 +11,26 @@ submitBtn.addEventListener('click', (e) => {
     if (!inputBtn.validity.patternMismatch && !inputBtn.validity.valueMissing && !inputBtn.validity.typeMismatch) {
         const successSection = document.querySelector('.hide');
         const cardSection = document.querySelector('.card');
+        const emailResult = document.querySelector('.email-result');
+
+        console.log(inputBtn.textContent);
+        emailResult.textContent = `${inputBtn.value}`
         successSection.classList.remove('hide');
         cardSection.classList.add('hide');
+    } else {
+        emailError.textContent = 'Valid email required';
     }
+
+    
     
 });
+
+inputBtn.addEventListener("input", (e) => {
+
+    if (inputBtn.validity.valid) {
+        emailError.textContent = ''; 
+    }
+})
 
 dismissBtn.addEventListener('click', (e) => {
     const successSection = document.querySelector('.success-card-con');
