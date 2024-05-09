d985a38b8a32ddd1a54a545d2a67034b9296a08d
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-05-08 18:15:12 -0700

    add code to show success component and dismiss component

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
 const submitBtn = document.querySelector('.sub-btn');
+const dismissBtn = document.querySelector('.dismiss-btn');
 
 submitBtn.addEventListener('click', (e) => {
+    // validate emial here
+
+    const successSection = document.querySelector('.hide');
+    const cardSection = document.querySelector('.card');
+
     
+    successSection.classList.remove('hide');
+    cardSection.classList.add('hide');
+});
+
+dismissBtn.addEventListener('click', (e) => {
+    const successSection = document.querySelector('.success-card-con');
+    const cardSection = document.querySelector('.card');
+
+    successSection.classList.add('hide');
+    cardSection.classList.remove('hide');
 })
